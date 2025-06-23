714c1078ea103c7cb6f2fa2004f4570963050b8a
Abinash4567 <stickatstack@outlook.com>
2025-06-23 14:35:10 +0530

    fix: vercel issue

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth/";
 import GithubProvider from "next-auth/providers/github";
 import { db } from "@/lib/prisma";
 
